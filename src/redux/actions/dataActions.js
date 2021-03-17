3ef0528f9db245d2d6845109d8adec26c93306fd
egoheru <egoherokugit@gmail.com>
2021-03-17 12:25:57 +0100

    updated

---
+++ src/redux/actions/dataActions.js
@@ -0,0 +1,60 @@
+import { 
+    SET_SCREAMS, 
+    LOADING_DATA, 
+    LIKE_SCREAM, 
+    UNLIKE_SCREAM,
+    DELETE_SCREAM 
+} from '../types';
+import axios from 'axios';
+
+// Get all screams
+export const getScreams = () => dispatch => {
+  dispatch({ type: LOADING_DATA });
+  axios
+   .get('https://europe-west1-socialappe-72b1d.cloudfunctions.net/api/screams')
+    .then((res) => {
+        dispatch({
+            type: SET_SCREAMS,
+            payload: res.data
+        })
+    })
+    .catch((err) => {
+        dispatch({
+            type: SET_SCREAMS,
+            payload: []
+        })
+    })
+}
+
+// Like a scream
+export const likeScream = (screamId) => (dispatch) => {
+    axios
+     .get(`https://europe-west1-socialappe-72b1d.cloudfunctions.net/api/scream/${screamId}/like`)
+       .then((res) => {
+           dispatch({
+               type: LIKE_SCREAM,
+               payload: res.data
+           });
+       })
+       .catch((err) => console.log(err));
+};
+// Unlike a scream
+export const unlikeScream = (screamId) => (dispatch) => {
+    axios
+     .get(`https://europe-west1-socialappe-72b1d.cloudfunctions.net/api/scream/${screamId}/unlike`)
+       .then((res) => {
+           dispatch({
+               type: UNLIKE_SCREAM,
+               payload: res.data
+           });
+       })
+       .catch((err) => console.log(err));
+}
+
+export const deleteScream = (screamId) => (dispatch) => {
+    axios.delete(`https://europe-west1-socialappe-72b1d.cloudfunctions.net/api/scream/${screamId}`)
+      .then(() => {
+         dispatch({ type: DELETE_SCREAM, payload: screamId})
+      })
+      .catch((err) => console.log(err));
+}
